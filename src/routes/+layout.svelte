5a825f29af56e0938124f496c1ddbf3ebd2ce61a
Riasat Sk <riasatofficial@outlook.com>
2024-06-18 21:59:50 +0530

    Changes to be committed: 	modified:   src/routes/+layout.svelte 	modified:   src/routes/+page.svelte 	modified:   tailwind.config.js

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
-  import "../app.css";
+	import '../app.css';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const theme = localStorage.getItem('theme') || 'cupcake';
+		document.documentElement.setAttribute('data-theme', theme);
+	});
 </script>
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+</script>
+
+<div class="navbar bg-base-100">
+	<a class="btn btn-ghost text-xl">Krittim</a>
+</div>
