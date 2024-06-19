d1ad0b4fa2ec703cff26d7aaa4427b263d8aca10
Riasat Sk <riasatofficial@outlook.com>
2024-06-19 11:50:32 +0530

    modified:   src/routes/+page.svelte 	new file:   static/student.png

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,10 +9,11 @@
 
 
 <div class="hero min-h-screen bg-base-200">
-  <div class="hero-content text-center">
-    <div class="max-w-md">
+  <div class="hero-content flex-col lg:flex-row">
+    <img src="/student.png" class="max-w-sm rounded-lg shadow-2xl" />
+    <div>
       <h1 class="text-5xl font-bold">Krittim School</h1>
-      <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <p class="py-6">Manage your school digital</p>
       <button class="btn btn-primary">Get Started</button>
     </div>
   </div>
